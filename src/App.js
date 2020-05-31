675ed7ba0845818f51e0e8af07e1e57aa567d24d
Luís Henrique <terytery98@gmail.com>
2020-05-31 20:02:36 -0300

    SideMenu Template

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import Header from './components/Header'
+import SideNav from './components/SideNav'
 import GlobalStyle from './styles/global'
 function App() {
   return (
     <>
     <Header />
+    <div class='col-lg-2 left'>
+    <SideNav/>
+    </div>
 
     <BrowserRouter>
       <Routes />
---
+++ src/components/SideNav/index.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import { Collapse,NavbarToggler} from 'reactstrap';
+import {SideMenu} from './styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faNewspaper,faAward,faPlusCircle,faTrophy,faUsers,faAddressCard,faUsersCog,faStar,faScroll} from '@fortawesome/free-solid-svg-icons'
+
+function SideNav(){
+
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => {if (!collapsed){setIsOpen(false);setIsOpen2(false);setIsOpen3(false)};  setCollapsed(!collapsed)}
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () =>{if (!isOpen){setIsOpen2(false);setIsOpen3(false)};
+                                  setIsOpen(!isOpen);}
+
+  const [isOpen2, setIsOpen2] = useState(false);
+  
+  const toggle2 = () => setIsOpen2(!isOpen2);
+
+  const [isOpen3, setIsOpen3] = useState(false);
+  
+  const toggle3= () => setIsOpen3(!isOpen3);
+return (
+
+<SideMenu navbar='true'>
+<NavbarToggler onClick={toggleNavbar}><FontAwesomeIcon icon={faScroll}/></NavbarToggler>
+<Collapse isOpen={!collapsed} navbar>
+<div class="container">
+  <div class="card">
+    <div class="body">
+      <ul>
+        <li > <FontAwesomeIcon icon={faNewspaper} /> Avisos</li>
+        <li><FontAwesomeIcon icon={faAward}/> Recompensas</li>
+        <li onClick={toggle}><FontAwesomeIcon icon={faPlusCircle}/> Cadastrar</li>
+        <Collapse isOpen={isOpen}>
+        
+        <li class='li2'> <FontAwesomeIcon icon={faNewspaper} /> Avisos</li>
+        <li class='li2 'onClick={toggle2}><FontAwesomeIcon icon={faTrophy}/> Prêmios</li>
+        <Collapse isOpen={isOpen && isOpen2}>
+        <li class='li3'><FontAwesomeIcon icon={faStar}/> Conquistas</li>
+        <li class='li3'><FontAwesomeIcon icon={faAward}/> Recompensas</li>
+        </Collapse>
+
+        <li class='li2'onClick={toggle3}><FontAwesomeIcon icon={faUsers}/> Equipes</li>
+
+        <Collapse isOpen={isOpen && isOpen3}>
+     
+        <li class='li3' ><FontAwesomeIcon icon={faAddressCard}/> Cargo</li>
+        <li class='li3'><FontAwesomeIcon icon={faUsersCog}/> Setor</li>
+     
+        </Collapse>
+        </Collapse>
+            
+      </ul>
+    </div>
+  </div>
+</div>
+</Collapse>
+</SideMenu>
+
+  /* <SideMenu vertical navbar='true'>
+  <NavItem> 
+  <Button href='#'><b>Avisos</b></Button>
+    </NavItem>
+  
+  <NavItem>
+  <Button href='#' size='sm'><b>Recompensas</b></Button>
+    </NavItem>
+
+    <NavItem>
+  <Button  onClick={toggle} ><b>Cadastrar</b></Button>
+    </NavItem>
+
+   
+    <NavItem>
+      <Button href='#'><b>Avisos</b></Button>
+    </NavItem>
+
+    <NavItem>
+    <Button  onClick={toggle2}><b>Prêmios</b></Button>
+    </NavItem>
+
+    <Collapse isOpen={isOpen && isOpen2}>
+    <NavItem>
+   <Button href='#'><b>Conquistas</b></Button>
+    </NavItem>
+    <NavItem>
+   <Button href='#'><b>Recompensas</b></Button>
+    </NavItem>
+
+    </Collapse>
+
+    <NavItem>
+    <Button  onClick={toggle3}><b>Estruturas</b></Button>
+    </NavItem>
+
+    <NavItem>
+    <Button href='#'><b>Cargo</b></Button>
+    </NavItem>
+    <NavItem>
+   <Button href='#'><b>Setor</b></Button>
+    </NavItem>
+
+    </Collapse>
+    </Collapse>
+
+</SideMenu>
+ */
+
+);
+}
+
+export default SideNav;
+
