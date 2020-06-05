5a9def818ccd296b97f76db036eda5c3ba3b46de
Luís Henrique <terytery98@gmail.com>
2020-06-05 04:31:40 -0300

    Cadastrar Conquistas

--- src/pages/Cadastros/Conquistas/index.js
+++ src/pages/Cadastros/Conquistas/index.js
@@ -1,46 +1,29 @@
 import React from 'react';
 import { Container, Row, Col,Jumbotron, } from 'reactstrap';
 import {Base} from './style' ;
-import {CadastroConquistas} from '.../components/CadastrarConquistas'
-
+import CadastroConquista from '../../../components/CadastroConquista';
 
 function Conquistas() {
   return(
 
- <Base>
+
 
 <Col xs="12" >
-<h1>Teste</h1>
 <Row>
-
-<Col xs="2">
+<Col xs="12">
 <Container>
-
+<CadastroConquista>
+</CadastroConquista>
 
     </Container>
   </Col>
-
-  <Col xs="7">
-<Container>
-
-
-    </Container>
-  </Col>
-
 
   
-  <Col xs="3">
-<Container>
-
-
-    </Container>
-  </Col>
-
   </Row>
  
 </Col>
 
-</Base>
+
   );
 }
 
