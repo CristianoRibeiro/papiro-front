995cfabf86bf0086739ea24c1bdda5763340b595
Luís Henrique <terytery98@gmail.com>
2020-06-07 00:59:53 -0300

    Correção de baiano

--- src/pages/Cadastros/Conquistas/index.js
+++ src/pages/Cadastros/Conquistas/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Container, Row, Col,Jumbotron, } from 'reactstrap';
 import {Base} from './style' ;
-<<<<<<< HEAD
-import {CadastroConquistas} from './../../../components/CadastrarConquista'
-=======
 import CadastroConquista from '../../../components/Conquistas/CadastroConquista'
 import ConsultarConquistas from '../../../components/Conquistas/ConsultarConquistas'
 
->>>>>>> 38f7685ebb8718e5ab1d0791d1013fef1737c6c4
+
 
 
 function Conquistas() {
