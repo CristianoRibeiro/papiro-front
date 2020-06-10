501a0ce2688588fbc25d55b1830c117790788a36
Cristiano Ribeiro <cristianoribeiro@Cristianos-MacBook-Pro.local>
2020-06-10 15:10:27 -0300

    aviso

--- src/pages/Cadastros/Avisos/index.js
+++ src/pages/Cadastros/Avisos/index.js
@@ -1,21 +1,124 @@
-import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import React, { useState } from 'react';
+import {
+  Button,
+  Container,
+  Col,
+  Row
+} from 'reactstrap';
 
-function Avisos() {
-  return(
+import { Form, Input ,Textarea } from '@rocketseat/unform';
+import Header from './../../../components/Header';
+
+import {
+  ContainerBG,
+  Userinfo,
+  Avatar,
+  Postinfobot,
+  PosttextComent,
+} from './styles';
+
+import api from '../../../services/api';
+
+import { getIdUsuario } from "../../../services/auth";
+import * as Yup from 'yup';
+
+
+import avatar from '../../../images/user.png';
+
+function Avisos(props) {
+
+  const [error, setError] = useState('');
+
+
+
+  const schema = Yup.object().shape({
+    titulo: Yup.string().required('O titulo do Aviso é obrigatório'),
+    mensagem: Yup.string().required('Digite sua mensagem do Aviso.')
+  });
+
+
+
+  async function handlerSubmit(data, {setSubmitting, resetForm}){
+
+    try {
+
+
+      let response = await api.post('/Aviso/Cadastrar', {
+        TituloAviso: data.titulo,
+        Aviso: data.mensagem,
+        IsAtivo:true,
+        CdUsuario: getIdUsuario()
+      });
+      resetForm({})
+
+    } catch (error) {
+      setSubmitting(false)
+      setError('Error com a conexão!')
+
+    }
+
+  }
+
+  return (
+    <>
+    <Header  {...props} />
+
     <Container>
       <Row>
-        <Col xs="4">
-        </Col>
-        <Col xs="5">
-        <h1>Teste</h1>
-        </Col>
-        <Col xs="3">
-        
-        </Col>
-      </Row>
+
+
+      <Col sm="12" md={{ size: 8, offset: 2 }}>
+
+
+      <legend>Cadastrar Aviso</legend>
+
+      <ContainerBG>
+      <Form className="form" schema={schema} onSubmit={handlerSubmit} >
+          <div className="topwrap">
+            <Userinfo>
+                <Avatar>
+                    <img src={avatar} alt="" />
+                </Avatar>
+
+
+            </Userinfo>
+            <PosttextComent>
+                  <div className="textwraper">
+                      {error && (<div className="error">Error ao inserir</div>)}
+                      <div className="postreply">Titulo</div>
+                      <Input name="titulo" id="title"  placeholder="Titulo do aviso" />
+                      <div className="postreply">Postar um novo aviso</div>
+                      <Textarea name="mensagem" id="reply" placeholder="Digite aqui seu aviso" />
+                  </div>
+              </PosttextComent>
+              <div className="clearfix"></div>
+          </div>
+          <Postinfobot>
+
+
+
+              <div className="pull-right postreply">
+                  <div className="pull-left smile"><a href="#"><i className="fa fa-smile-o"></i></a></div>
+                  <div className="pull-left">
+                  <Button className="ml-2 mr-4 btn-success btn-sm" type="submit" >
+                      Publicar
+                  </Button>
+                  </div>
+                  <div className="clearfix"></div>
+              </div>
+
+
+              <div className="clearfix"></div>
+          </Postinfobot>
+      </Form>
+    </ContainerBG>
+
+    </Col>
+    </Row>
     </Container>
-  );
+
+    </>
+    );
 }
 
 export default Avisos;
---
+++ src/pages/Cadastros/Avisos/styles.js
@@ -0,0 +1,262 @@
+import styled from 'styled-components';
+
+export const ContainerBG = styled.div`
+    background-color: #ffffff;
+    border-radius: 2px;
+    box-shadow: 0 1px 2px #c9cccd;
+    margin-bottom: 20px;
+    display: block;
+
+    .clearfix:before {
+      content: " ";
+      display: table;
+    }
+
+    .form {
+      display: block;
+      margin-top: 0em;
+      .topwrap{
+        display: flex;
+      }
+    }
+`;
+export const ContentLeft = styled.div`
+  float: left!important;
+  width: 85%;
+  box-sizing: border-box;
+  display: flex;
+
+  .clearfix:before {
+      content: " ";
+      display: table;
+    }
+`;
+
+export const ContentLeftAll = styled.div`
+  float: left!important;
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+
+  .clearfix:before {
+      content: " ";
+      display: table;
+    }
+`;
+
+
+export const Userinfo = styled.div`
+    width: 15%;
+    padding: 20px 0 15px 15px;
+`;
+
+
+export const Avatar = styled.div`
+    width: 37px;
+    margin-left: 5px;
+    position: relative;
+    img {
+      width: 100%;
+    }
+`;
+
+
+export const Posttext = styled.div`
+    display: block;
+    width: 100%;
+    padding-right: 30px;
+    padding-top: 20px;
+    padding-bottom: 15px;
+    color: #989c9e;
+    font-size: 14px;
+    font-family: 'Open Sans Light', sans-serif;
+    line-height: 25px;
+
+    h2 {
+    color: #363838;
+    font-size: 18px;
+    font-family: 'Open Sans', sans-serif;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    }
+
+    p {
+      margin: 0 0 10px;
+    }
+
+   blockquote {
+        border: solid 1px #f1f1f1;
+        border-radius: 2px;
+        font-size: 14px;
+        padding: 18px;
+
+        .original, .postreply {
+            font-size: 12px;
+            color: #bdc3c7;
+            display: block;
+            font-family: 'Open Sans Regular', sans-serif;
+        }
+    }
+
+
+`;
+
+
+
+
+
+export const Postinfo = styled.div`
+width: 15%;
+    border-left: solid 1px #f1f1f1;
+    display: flex;
+    flex-direction: column;
+`;
+
+
+export const Comments = styled.div`
+    border-bottom: solid 1px #f1f1f1;
+    padding: 18px 0 25px 0;
+    text-align: center;
+
+`;
+
+
+export const Commentbg = styled.div`
+    background-color: #bdc3c7;
+    border-radius: 2px;
+    display: inline-block;
+    padding: 12px 17px;
+    color: #ffffff;
+    font-size: 14px;
+    font-family: 'Open Sans Bold', sans-serif;
+    position: relative;
+`;
+
+
+export const Mark = styled.div`
+    width: 11px;
+    height: 11px;
+    background-color: #bdc3c7;
+    position: absolute;
+    bottom: 0;
+    left: 43%;
+    margin-bottom: -5px;
+    transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+`;
+
+
+export const Views = styled.div`
+    border-bottom: solid 1px #f1f1f1;
+    color: #9da6aa;
+    font-size: 12px;
+    font-family: 'Open Sans Regular', sans-serif;
+    text-align: center;
+    line-height: 29px;
+`;
+
+
+export const Time = styled.div`
+    color: #9da6aa;
+    font-size: 12px;
+    font-family: 'Open Sans Regular', sans-serif;
+    text-align: center;
+    line-height: 29px;
+`;
+
+export const Postinfobot = styled.div`
+    border-top: solid 1px #f1f1f1;
+    line-height: 50px;
+    padding: 0 30px 0 94px;
+    color:#999;
+
+    .pull-left {
+      float: left;
+    }
+
+    .likeblock {
+       width: 15%;
+    }
+    .up {
+      color: #1abc9c;
+      font-size: 12px;
+    }
+    .down {
+        color: #db7a7a;
+        font-size: 12px;
+        margin-left: 20px;
+    }
+    .prev {
+        width: 10%;
+        margin-left: 20px;
+    }
+    .posted {
+        width: 50%;
+        margin-left: 18px;
+        font-size: 12px;
+        color: #999;
+        font-family: 'Open Sans Regular', sans-serif;
+    }
+    .next {
+        width: 10%;
+        text-align: right;
+    }
+    .pull-right {
+      float: right!important;
+  }
+`;
+
+
+
+export const PosttextComent = styled.div`
+    display: block;
+    width: 80%;
+    padding-right: 30px;
+    padding-top: 20px;
+    padding-bottom: 15px;
+    color: #989c9e;
+    font-size: 14px;
+    font-family: 'Open Sans Light', sans-serif;
+    line-height: 25px;
+
+    .postreply {
+      font-size: 12px;
+      color: #000;
+      font-weight: bold;
+      display: block;
+      font-family: 'Open Sans Regular', sans-serif;
+    }
+  .textwraper {
+    background-color: #f1f1f1;
+    border-radius: 2px;
+    padding: 18px;
+    textarea, input {
+    border: none;
+    box-shadow: none;
+    background-color: #f1f1f1;
+    /* width: 610px; */
+    width: 100%;
+
+    }
+
+    span {
+        margin-top: 0px;
+        font-size: 10px;
+        color: red;
+        font-weight: bold;
+      }
+
+    .error {
+      color: #f00;
+      font-style: italic;
+    }
+
+}
+
+`;
+
+
+
+
+
